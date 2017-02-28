8e305bbc0bbc2ab7b2a715e88e876c124bb32b0b
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-02-28 18:18:20 +0200

    squash! simplify update-authors.js
    * Don't use a Set together with an array, O(n^2) instead of O(n*log(n))
      is fine for such tool but the code is shorter.
    * Use a regular for-of loop instead of reduce.

--- tools/update-authors.js
+++ tools/update-authors.js
@@ -8,14 +8,12 @@ const { getCommandOutput, writeFile } = require('./common');
 const AUTHORS_PATH = path.resolve(__dirname, '..', 'AUTHORS');
 
 getCommandOutput('git log --reverse --format="%aN <%aE>"').then((out) => {
-  const seen = new Set();
-  const authors = out.split('\n').reduce((list, author) => {
-    if (!seen.has(author)) {
-      list.push(author);
-      seen.add(author);
+  const authors = [];
+  for (const author of out.split('\n')) {
+    if (!authors.includes(author)) {
+      authors.push(author);
     }
-    return list;
-  }, []);
+  }
   return writeFile(AUTHORS_PATH, authors.join('\n'));
 }).catch((error) => {
   const message = error.stack || error.toString();
