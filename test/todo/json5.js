c271d1fd7925e76ad68b4ea106b4acd5ec0a37bc
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-05-12 21:52:58 +0300

    squash! add noctal tests to supportedByUs

--- test/todo/json5.js
+++ test/todo/json5.js
@@ -9,6 +9,12 @@ const supportedByUs = {
   arrays: [
     'leading-comma-array',
     'lone-trailing-comma-array'
+  ],
+  numbers: [
+    'negative-noctal',
+    'noctal-with-leading-octal-digit',
+    'noctal',
+    'positive-noctal'
   ]
 };
 
