d30340aafcc554742d2d36d4937e124b1f321bb7
Dmytro Nechai <nechaido@gmail.com>
2017-05-15 18:51:37 +0300

    test: remove subtests from serde tests

--- test/node/serde.js
+++ test/node/serde.js
@@ -7,22 +7,16 @@ const jstp = require('../..');
 const testCases = require('../fixtures/serde.test-cases');
 
 testCases.serde.concat(testCases.serialization).forEach((testCase) => {
-  tap.test(`must serialize ${testCase.name}`, (test) => {
-    test.strictSame(jstp.stringify(testCase.value), testCase.serialized);
-    test.end();
-  });
+  tap.strictSame(jstp.stringify(testCase.value), testCase.serialized,
+    `must serialize ${testCase.name}`);
 });
 
 testCases.serde.concat(testCases.deserialization).forEach((testCase) => {
-  tap.test(`must deserialize ${testCase.name}`, (test) => {
-    test.strictSame(jstp.parse(testCase.serialized), testCase.value);
-    test.end();
-  });
+  tap.strictSame(jstp.parse(testCase.serialized), testCase.value,
+    `must deserialize ${testCase.name}`);
 });
 
 testCases.invalidDeserialization.forEach((testCase) => {
-  tap.test(`must not allow ${testCase.name}`, (test) => {
-    test.throws(() => jstp.parse(testCase.serialized));
-    test.end();
-  });
+  tap.throws(() => jstp.parse(testCase.serialized),
+    `must not allow ${testCase.name}`);
 });
