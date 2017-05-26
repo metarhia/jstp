48319a6e1fa84855ee65e97469df1ffe8acd9b26
Dmytro Nechai <nechaido@gmail.com>
2017-05-26 20:37:04 +0300

    test: remove unnecessary subtests

--- test/node/remote-error-default-messages.js
+++ test/node/remote-error-default-messages.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const test = require('tap').test;
+const test = require('tap');
 
 const jstp = require('../..');
 const RemoteError = jstp.RemoteError;
@@ -9,7 +9,5 @@ const expectedMessages = Object.keys(jstp)
   .filter(key => key.startsWith('ERR_'))
   .map(key => jstp[key].toString());
 
-test('Must have a default message for every predefined error', (test) => {
-  test.includes(Object.keys(RemoteError.defaultMessages), expectedMessages);
-  test.end();
-});
+test.includes(Object.keys(RemoteError.defaultMessages), expectedMessages,
+  'Must have a default message for every predefined error');
