ac411d35bfdb6be6f3ac283141e5ab205d4b8935
Dmytro Nechai <nechaido@gmail.com>
2017-05-29 19:57:42 +0300

    fixup! test: add requested changes

--- test/node/common-safe-require.js
+++ test/node/common-safe-require.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const tap = require('tap');
+const test = require('tap');
 
 const common = require('../../lib/common');
 
-const existingModule = '../..';
+const existingModule = 'fs';
 const nonExistingModule = '__non_existing_module__';
 
-tap.ok(common.safeRequire(existingModule), 'must require existing module');
+test.ok(common.safeRequire(existingModule), 'must require existing module');
 
-tap.equal(common.safeRequire(nonExistingModule), null,
+test.equal(common.safeRequire(nonExistingModule), null,
   'must return \'null\' if module doesn\'t exist');
