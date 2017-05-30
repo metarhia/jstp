bb56c7ad1f53e4b9afce508f86d0ea0721b93de2
Dmytro Nechai <nechaido@gmail.com>
2017-05-30 19:04:57 +0300

    test: migrate event and remote-proxy tests

---
+++ test/node/connection-event.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const test = require('tap');
+
+const jstp = require('../..');
+
+const name = 'testApp';
+
+const app = {
+  name,
+  interfaces: {}
+};
+
+const application = new jstp.Application(app.name, app.interfaces);
+let server;
+let client;
+
+test.beforeEach((done) => {
+  const port = 3336;
+  server = jstp.tcp.createServer(port, [application], app.authCallback);
+  server.listen(() => {
+    client = jstp.tcp.createClient({ host: 'localhost', port });
+    done();
+  });
+});
+
+test.afterEach((done) => {
+  client.disconnect();
+  server.close();
+  done();
+});
+
+test.test('client must process an event', (test) => {
+  test.plan(2);
+
+  client.connectAndHandshake(app.name, null, null, (error, connection) => {
+    test.assertNot(error, 'must be no error');
+
+    const iface = 'someInterface';
+    const eventName = 'someEvent';
+    const args = ['firstArgument', 'secondArgument'];
+
+    connection.on('event', (event) => {
+      if (event.remoteEventName === eventName) {
+        test.strictDeepEqual(event.remoteEventArgs, args);
+      } else {
+        test.fail(`unexpected event: ${event.remoteEventName}`);
+      }
+    });
+
+    server.getClients()[0].emitRemoteEvent(iface, eventName, args);
+  });
+});
