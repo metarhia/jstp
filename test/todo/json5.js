dbbebd1f96725012c4a7bd59dd25f5ce157b57eb
Dmytro Nechai <nechaido@gmail.com>
2017-06-02 19:41:35 +0300

    fixup! test: compare results only if no error is raised

--- test/todo/json5.js
+++ test/todo/json5.js
@@ -62,22 +62,22 @@ testCases.forEach((testCase) => {
         } else if (ext === '.json5') {
           let value;
           test.doesNotThrow(() => value = jstp.parse(file));
-          if (!test.passing) {
+          if (test.passing()) {
             const expected = extendedEval(file);
             test.assert(deepEqual(value, expected));
             if (!test.passing())
-              test.comment(difference(value, expected));
+              console.worn(difference(value, expected));
           }
         } else if (ext === '.js') {
           const supportedTests = supportedByUs[testCase.name];
           if (supportedTests && supportedTests.includes(testName)) {
             let value;
             test.doesNotThrow(() => value = jstp.parse(file));
-            if (!test.passing) {
+            if (test.passing()) {
               const expected = extendedEval(file);
               test.assert(deepEqual(value, expected));
               if (!test.passing())
-                test.comment(difference(value, expected));
+                console.warn(difference(value, expected));
             }
           } else {
             test.throws(() => jstp.parse(file));
