6c7ef2faea88a95146863ea5fca711f8512238f2
Denys Otrishko <shishugi@gmail.com>
2017-06-22 12:17:00 +0300

    lib: implement API versioning
    * allow to specify API version during handshake
    * make createAppsIndex handle multiple API versions
    * add API version parameter to Application
    * add tests for interface versioning

---
+++ test/node/api-versions.js
@@ -0,0 +1,198 @@
+'use strict';
+
+const test = require('tap');
+
+const jstp = require('../..');
+
+const app = require('../fixtures/application');
+
+const interfacesV1 = {
+  calculator: {
+    answer(connection, callback) {
+      callback(null, 42);
+    },
+  },
+};
+
+const interfacesV2 = {
+  calculator: {
+    answer(connection, callback) {
+      callback(null, 24);
+    },
+  },
+};
+
+const interfacesVLatest = {
+  calculator: {
+    answer(connection, callback) {
+      callback(null, 13);
+    },
+  },
+};
+
+const appV1 = new jstp.Application(app.name, interfacesV1, '1.0.0');
+const appV2 = new jstp.Application(app.name, interfacesV2, '2.0.0');
+const appVlatest = new jstp.Application(app.name, interfacesVLatest);
+
+let server;
+let connection;
+
+test.afterEach((done) => {
+  if (connection) {
+    connection.close();
+    connection = null;
+  }
+  if (server) server.close();
+  done();
+});
+
+test.test('must allow to specify version in application name', (test) => {
+  const appV1 = new jstp.Application('app@1.0.0', interfacesV1);
+  test.strictSame(appV1.version, '1.0.0');
+  test.end();
+});
+
+test.test('must handle specific versions', (test) => {
+  const serverConfig = {
+    applications: [appV1, appV2, appVlatest], authPolicy: app.authCallback,
+  };
+  server = jstp.net.createServer(serverConfig);
+  server.listen(0, () => {
+    test.plan(9);
+
+    const port = server.address().port;
+
+    // latest
+    const appLatest = { name: app.name };
+    jstp.net.connect(appLatest, null, port, (error, conn) => {
+      test.assertNot(error, 'connect must not return an error');
+      conn.callMethod('calculator', 'answer', [], (error, result) => {
+        test.assertNot(error, 'callMethod must not return an error');
+        test.strictSame(result, 13);
+        conn.close();
+      });
+    });
+
+    // v1
+    const appV1 = { name: app.name, version: '1' };
+    jstp.net.connect(appV1, null, port, (error, conn) => {
+      test.assertNot(error, 'connect must not return an error');
+      conn.callMethod('calculator', 'answer', [], (error, result) => {
+        test.assertNot(error, 'callMethod must not return an error');
+        test.strictSame(result, 42);
+        conn.close();
+      });
+    });
+
+    // v2
+    const appV2 = { name: app.name, version: '2' };
+    jstp.net.connect(appV2, null, port, (error, conn) => {
+      test.assertNot(error, 'connect must not return an error');
+      conn.callMethod('calculator', 'answer', [], (error, result) => {
+        test.assertNot(error, 'callMethod must not return an error');
+        test.strictSame(result, 24);
+        conn.close();
+      });
+    });
+  });
+});
+
+test.test('must handle version ranges', (test) => {
+  const serverConfig = {
+    applications: [appV1, appV2, appVlatest], authPolicy: app.authCallback,
+  };
+  server = jstp.net.createServer(serverConfig);
+  server.listen(0, () => {
+    test.plan(6);
+
+    const port = server.address().port;
+
+    // ^1.0.0
+    const appV1Compatible = { name: app.name, version: '^1.0.0' };
+    jstp.net.connect(appV1Compatible, null, port, (error, conn) => {
+      test.assertNot(error, 'connect must not return an error');
+      conn.callMethod('calculator', 'answer', [], (error, result) => {
+        test.assertNot(error, 'callMethod must not return an error');
+        test.strictSame(result, 42);
+        conn.close();
+      });
+    });
+
+    // > v1.0.0
+    const appV1Higher = { name: app.name, version: '>1.0.0' };
+    jstp.net.connect(appV1Higher, null, port, (error, conn) => {
+      test.assertNot(error, 'connect must not return an error');
+      conn.callMethod('calculator', 'answer', [], (error, result) => {
+        test.assertNot(error, 'callMethod must not return an error');
+        test.strictSame(result, 24);
+        conn.close();
+      });
+    });
+  });
+});
+
+test.test('must return an error on connect to nonexistent version', (test) => {
+  const serverConfig =
+    { applications: [appV1, appV2, appVlatest], authPolicy: app.authCallback };
+  server = jstp.net.createServer(serverConfig);
+  server.listen(0, () => {
+    const port = server.address().port;
+    const nonexistentApp = { name: app.name, version: '9999' };
+    jstp.net.connect(nonexistentApp, null, port, (error) => {
+      test.assert(error, 'connect must return an error');
+      test.equal(error.code, jstp.ERR_APP_NOT_FOUND,
+        'error must be an ERR_APP_NOT_FOUND');
+      test.end();
+    });
+  });
+});
+
+test.test('must return an error on connect to invalid version', (test) => {
+  const serverConfig =
+    { applications: [appV1, appV2, appVlatest], authPolicy: app.authCallback };
+  server = jstp.net.createServer(serverConfig);
+  server.listen(0, () => {
+    const port = server.address().port;
+    const application = { name: app.name, version: '__invalid_version__' };
+    jstp.net.connect(application, null, port, (error) => {
+      test.assert(error, 'connect must return an error');
+      test.equal(error.message, 'Invalid semver version range');
+      test.end();
+    });
+  });
+});
+
+test.test('must set biggest version as latest versions', (test) => {
+  const serverConfig =
+    { applications: [appV1, appV2], authPolicy: app.authCallback };
+  server = jstp.net.createServer(serverConfig);
+  server.listen(0, () => {
+    const port = server.address().port;
+    jstp.net.connect(app.name, null, port, (error, conn) => {
+      connection = conn;
+      connection.callMethod('calculator', 'answer', [], (error, result) => {
+        test.assertNot(error, 'callMethod must not return an error');
+        test.strictSame(result, 24);
+        test.end();
+      });
+    });
+  });
+});
+
+test.test('must throw an error on invalid version in application name',
+  (test) => {
+    test.throws(() => {
+      new jstp.Application('app@__invalid__', interfacesV1);
+    }, TypeError, 'Invalid semver version');
+    test.end();
+  }
+);
+
+test.test('must throw an error on invalid version in application parameter',
+  (test) => {
+    test.throws(() => {
+      new jstp.Application('app', interfacesV1, '__invalid__');
+    }, TypeError, 'Invalid semver version');
+    test.end();
+  }
+);
